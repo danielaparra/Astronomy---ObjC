15dbd52704594179cad4d03001eeb46f56fcbff9
Daniela Parra <dyparra47@gmail.com>
2018-11-26 18:08:57 -0500

    Set up storybboard with collection view and detail view scenes.

---
+++ AstronomyObjC/AstronomyObjC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AstronomyObjC
+//
+//  Created by Daniela Parra on 11/26/18.
+//  Copyright © 2018 Daniela Parra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

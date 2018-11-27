822eea2dc37e2a68964b7ec9a1b42fab55035bba
Daniela Parra <dyparra47@gmail.com>
2018-11-26 19:10:58 -0500

    Add save photo functionality to detail view.

--- AstronomyObjC/AstronomyObjC/View Controllers/PhotoDetailViewController.swift
+++ AstronomyObjC/AstronomyObjC/View Controllers/PhotoDetailViewController.swift
@@ -7,13 +7,66 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoDetailViewController: UIViewController {
+    
+    // MARK: - Lifecycle Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
+    
+    // MARK: - IBActions
+    
+    @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAsset(from: image)
+        }, completionHandler: { (success, error) in
+            if let error = error {
+                NSLog("Error saving photo: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.presentSuccessfulSaveAlert()
+            }
+        })
+    }
+    
+    // MARK: - Private Functions
+    
+    private func updateViews() {
+        
+    }
+    
+    private func presentSuccessfulSaveAlert() {
+        let alert = UIAlertController(title: "Photo Saved!", message: "The photo has been saved to your Photo Library!", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        
+        alert.addAction(okayAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Properties
+    
+//    var photo: MarsPhotoReference? {
+//        didSet {
+//            updateViews()
+//        }
+//    }
 
+    lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .short
+        return df
+    }()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var cameraLabel: UILabel!
+    
 }
